25878fcfea7b6096cf68e95dbd333fb403699830
leandramusto@gmail.com <leandramusto@gmail.com>
2024-03-14 13:48:22 +0100

    creating a list of active contact with v-if a nd v-elese

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,10 +7,137 @@ createApp({
         {
           name: "Michele",
           avatar: "assets/img/avatar_1.jpg",
-          text: "Last message sent",
-          hour: "12",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Hai portato a spasso il cane?",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "Ricordati di stendere i panni",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 16:15:22",
+              message: "Tutto fatto!",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Fabio",
+          avatar: "assets/img/avatar_2.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "20/03/2020 16:30:00",
+              message: "Ciao come stai?",
+              status: "sent",
+            },
+            {
+              date: "20/03/2020 16:30:55",
+              message: "Bene grazie! Stasera ci vediamo?",
+              status: "received",
+            },
+            {
+              date: "20/03/2020 16:35:00",
+              message: "Mi piacerebbe ma devo andare a fare la spesa.",
+              status: "sent",
+            },
+          ],
+        },
+        {
+          name: "Samuele",
+          avatar: "assets/img/avatar_3.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "28/03/2020 10:10:40",
+              message: "La Marianna va in campagna",
+              status: "received",
+            },
+            {
+              date: "28/03/2020 10:20:10",
+              message: "Sicuro di non aver sbagliato chat?",
+              status: "sent",
+            },
+            {
+              date: "28/03/2020 16:15:22",
+              message: "Ah scusa!",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Alessandro B.",
+          avatar: "assets/img/avatar_4.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Lo sai che ha aperto una nuova pizzeria?",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "Si, ma preferirei andare al cinema",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Alessandro L.",
+          avatar: "assets/img/avatar_5.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Ricordati di chiamare la nonna",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "Va bene, stasera la sento",
+              status: "received",
+            },
+          ],
+        },
+        {
+          name: "Claudia",
+          avatar: "assets/img/avatar_6.jpg",
+          visible: true,
+          messages: [
+            {
+              date: "10/01/2020 15:30:55",
+              message: "Ciao Claudia, hai novità?",
+              status: "sent",
+            },
+            {
+              date: "10/01/2020 15:50:00",
+              message: "Non ancora",
+              status: "received",
+            },
+            {
+              date: "10/01/2020 15:51:00",
+              message: "Nessuna nuova, buona nuova",
+              status: "sent",
+            },
+          ],
         },
       ],
+      activeContact: null,
+      showChatWindow: false, // Aggiungi la proprietà showChatWindow e inizializzala a false
     };
+  },
+  methods: {
+    setActiveContact(contact) {
+      this.activeContact = contact;
+      this.showChatWindow = true; // Quando imposti un contatto attivo, mostra la finestra della chat
+    },
+    toggleChatWindow() {
+      this.showChatWindow = !this.showChatWindow; // Metodo per mostrare/nascondere la finestra della chat
+    },
   },
 }).mount("#app");
